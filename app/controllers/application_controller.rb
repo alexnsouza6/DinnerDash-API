003aa3b80840bf88d9aac317e272bbc2187deee5
alexnsouza6 <alexnsouza6@hotmail.com>
2018-11-25 18:10:39 -0200

    feat: method for generating token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::API
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+
+  protected
+
+  def generate_token(user)
+    secret = 'Dinnerdash-token-auth'
+    payload = { data: user.email + Time.new.to_s }
+    token = JWT.encode payload,
+                       secret,
+                       'HS256'
+    token
+  end
+
+  private
+
+  def authenticate_token
+    authenticate_with_http_token do |token, _options|
+      User.find_by_token(token)
+    end
+  end
 end
