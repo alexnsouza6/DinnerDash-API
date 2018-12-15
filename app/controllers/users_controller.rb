721962fe263b68fe0f7446790df911eabb55d783
alexnsouza6 <alexnsouza6@hotmail.com>
2018-12-15 21:12:59 -0200

    fix: add error to usercontroller#update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
     if @user.update(user_params)
       render json: @user, status: 200
     else
-      render json: { errors: @user.errors.full_messages }, status: 422
+      render json: { errors: "Couldn't save user correctly" }, status: 422
     end
   end
 
